c5c00474439086badcd2e2fa98681725039d8575
Aqib Khan <67043041+Aqib8860@users.noreply.github.com>
2025-04-08 15:08:48 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     # max_age=600 # for cache
 )
 
-allowed_host = ["localhost", "127.0.0.1", "3.110.154.148", "ec2-3-110-154-148.ap-south-1.compute.amazonaws.com"]
+allowed_host = ["localhost", "127.0.0.1", "3.110.154.148", "aiworld.solutions"]
 app.add_middleware(TrustedHostMiddleware, allowed_hosts=allowed_host)
 
 
